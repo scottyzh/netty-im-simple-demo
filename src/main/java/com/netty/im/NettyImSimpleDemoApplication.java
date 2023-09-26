befa8c1bc3f8c03e6974d8fe0f58e34ddb06ed62
scottyzh <656205698@qq.com>
2023-09-26 09:43:10 +0800

    初始框架，待完善

---
+++ src/main/java/com/netty/im/NettyImSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.netty.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyImSimpleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyImSimpleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netty/im/NettyImSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netty.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyImSimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
