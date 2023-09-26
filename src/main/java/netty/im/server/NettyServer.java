297c58344ea7bbec6030dd78471c49e6e6569243
scottyzh <656205698@qq.com>
2023-09-26 09:47:13 +0800

    初始框架搭建

---
+++ src/main/java/netty/im/server/NettyServer.java
@@ -0,0 +1,10 @@
+package netty.im.server;
+
+/********************************
+ *  @Class     : NettyServer 
+ *  @Function  : 
+ *  @Date      : 9:46 2023/9/26 
+ *  @Author    : GuoZiHuang
+ ********************************/
+public class NettyServer {
+}
